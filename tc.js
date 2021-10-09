934aafb5b83fd3abec433416e909a867494427c1
肥羊 <yunspeedtest@gmail.com>
2021-10-09 12:57:10 +0800

    delete

--- tc.js
+++
@@ -1,8 +0,0 @@
-layer.open({
-		    title: '<i class="layui-icon layui-icon-tips"> 温馨提示</i>'
-		    ,content: `
-		  <div style="text-align: center;">本站部署于广州腾讯云</div>
-		  <div style="text-align: center;">所以测速结果真实可靠</div>
-		  <div style="text-align: center;">若本站对你有帮助的话</div>
-		  <div style="text-align: center;">请订阅电报频道：<a href="https://t.me/yunspeedtest" style="color:red;text-decoration:none;font-size:20px">机场 ☁️ 测速</a></div>`
-		  });
